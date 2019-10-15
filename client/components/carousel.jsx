0826e9358a02abe2e031b155b3cf9ee7bfa5b2e0
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-15 22:04:49 +0000

    Added basic carousel in home screen with styling

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
 class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { index: 0 };
+    this.interval = null;
+    this.className = null;
+  }
+
+  goRight() {
+    if (this.state.index < 4) {
+      this.setState({ index: this.state.index + 1 });
+    } else {
+      this.setState({ index: 0 });
+    }
+  }
+
   render() {
+    let { index } = this.state;
+
+    clearInterval(this.interval);
+    this.interval = setInterval(() => {
+      this.goRight();
+    }, 3000);
+
     return (
-      <div className="main__carousel">
+      <div className={ `main__carousel background-${index}` }>
         <div className="main__carousel-header">
           GYM
         </div>
