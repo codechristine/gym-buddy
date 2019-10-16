230a61c6cd269d37543ee49852f2a2cb222b5785
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-16 19:54:00 +0000

    Defined setview method in app to allow for multiple elemnets to be rendered, modified element in render method to be dynamic based on current state, added initial skeleton files for map view component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import Header from './header';
 import Home from './home';
+import GymMap from './gym-map';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentUser: {},
-      name: 'home'
+      view: {
+        name: 'home',
+        params: {}
+      },
+      prevName: ''
     };
+    this.setView = this.setView.bind(this);
+  }
+  setView(name, params) {
+    this.setState({
+      view: {
+        name: name,
+        params: params
+      }
+    });
   }
   render() {
+    const { name } = this.state.view;
+    let element;
+    switch (name) {
+      case 'home':
+        element = <Home setView = { this.setView }/>;
+        break;
+      case 'map':
+        element = <GymMap setView = { this.setView } />;
+        break;
+    }
     return (
       <div className="main">
         <div className="main__container">
-          <Header />
-          <Home />
+          <Header name={ this.state.name }/>
+          { element }
         </div>
       </div>
     );
--- client/components/home.jsx
+++ client/components/home.jsx
@@ -4,10 +4,11 @@ import LocationSearchInput from './autocomplete';
 
 class Home extends React.Component {
   render() {
+    const viewMap = () => this.props.setView('map', {});
     return (
       <div className="main__body">
         <Carousel />
-        <div className="main__view">
+        <div className="main__view" onClick={ viewMap }>
           View Gyms Around Me
         </div>
         <LocationSearchInput />
