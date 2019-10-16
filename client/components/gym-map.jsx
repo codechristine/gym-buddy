f216ef8cdff740af72c7ffc9606623af09157d3b
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-16 21:47:21 +0000

    added basic styling for gym map component that includes map and list

--- client/components/gym-map.jsx
+++ client/components/gym-map.jsx
@@ -6,6 +6,12 @@ class GymMap extends React.Component {
     return (
       <div className="main__container">
         <Header name={this.props.view.name} prevName={this.props.view.prevName} setView={this.props.setView} />
+        <div className="main__body">
+          <div className="map__view">
+          </div>
+          <div className="map__list">
+          </div>
+        </div>
       </div>
     );
   }
