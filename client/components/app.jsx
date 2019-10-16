a1083b5d2af390f051f061e3bc742c5443731e9e
codechristine <christinele@csu.fullerton.edu>
2019-10-16 15:22:53 -0700

    rendered map onto homepage app file. Will need to move this later

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -12,12 +12,28 @@ export default class App extends React.Component {
     };
   }
   render() {
+
     return (
       <div className="main">
         <div className="main__container">
           <Header />
           <Home />
-          <GoogleMaps />
+          <GoogleMaps id="myMap"
+            options={{
+              center: { lat: 41.0082, lng: 28.9784 },
+              zoom: 8
+            }}
+            onMapLoad={map => {
+              const marker = new window.google.maps.Marker({
+                position: { lat: 41.0082, lng: 28.9784 },
+                map: map,
+                title: 'Hello Istanbul!'
+              });
+              marker.addListener('click', e => {
+                this.createInfoWindow(e, map);
+              });
+            }}
+          />
         </div>
       </div>
     );
