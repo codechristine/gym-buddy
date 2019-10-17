63cbd1af2e4a7381e880c45f24508173200d601c
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-17 04:33:24 +0000

    Added skeleton for sign up component, defined sign up component with basic styling for inner elements

---
+++ client/components/sign-up.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange() {
+
+  }
+  handleSubmit() {
+
+  }
+  render() {
+    return (
+      <h1>Test</h1>
+    );
+  }
+}
+
+export default SignUp;
