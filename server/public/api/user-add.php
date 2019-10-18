09f063a631e73da29d9e88c642ddf9e7567d5a09
NoelCarino <leononirac1992@gmail.com>
2019-10-18 01:00:23 +0000

    10/17 - 5:59pm | Back End - Added more php files

---
+++ server/public/api/user-add.php
@@ -0,0 +1,64 @@
+<?php
+    /*
+        The purpose of this file is to
+        add users to database
+    */
+
+?>
+
+
+<?php
+
+    $bodyData = getBodyData();
+
+
+    if( empty($bodyData) ){
+        throw new Exception( "Must enter user ID" )
+    }
+
+
+
+    if( $bodyData['id'] ){
+        $id = $bodyData['id'];
+    } else {
+        throw new Exception( 'Must have a valid product ID' );
+    }
+
+
+    if( intval( $id ) < 0 ){
+        throw new Exception( 'ID has to be hater than 0' )
+    } else if (!is_numeric($id)){
+        throw new Exception("not valid id")
+    }
+
+
+    $userQuery = "SELECT u.`id`
+                FROM `user` AS u
+                        WHERE u.`id` = $id";
+
+    $userResult = mysqli_query( $conn, $userQuery );
+
+    if( !$userResult ){
+        throw new Exception( 'Query result not valid');
+    }
+
+    $userData = [];
+
+    if( mysqli_num_rows( $userResult ) > 0 ){
+        while($userRow = mysqli_fetch_assoc( $userResult)){
+            $userData[] = $userRow;
+        }
+    } else { 
+        throw new Exception("Invalid userID: $id")
+    }
+    $user = intval($userData[0]['id'])
+
+    $friendRequest = mysqli_query($conn, "START FRIEND TRANSACTION");
+
+    if( !$friendRequest ) {
+        throw new Exeption(' Friend Request Error ');
+    }
+
+
+
+?>
