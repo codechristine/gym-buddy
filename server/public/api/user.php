99d133d88d9c14bda0e7344adb825d042f3f8a93
NoelCarino <leononirac1992@gmail.com>
2019-10-19 00:50:03 +0000

    Cleaning up back end side

---
+++ server/public/api/user.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once('functions.php');
+require_once('db-connection.php');
+
+startup();
+set_exception_handler('error_handler');
+
+switch($_SERVER['REQUEST_METHOD']){
+    case 'POST':
+        require_once('user-add.php');
+        break;
+    case 'GET':
+        require_once('user-get.php');
+        break;
+}
+
+
+
+
+?>
