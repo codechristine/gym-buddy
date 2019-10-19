2ce3756c01c382890f27e5e8447945afaf6e8925
codechristine <christinele.dev@gmail.com>
2019-10-18 19:24:23 -0700

    repushing after removing console

--- client/components/gym-view.jsx
+++ client/components/gym-view.jsx
@@ -1,45 +1,53 @@
 import React from 'react';
-// import { resolve } from 'path';
 import Header from './header';
 
 export default class GymView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      map: null
+      placeObject: null
     };
   }
-  render() {
+  componentDidMount() {
     const combinedMapObject = this.props.view.params;
 
-    // console.log(combinedMapObject);
-
     let request = {
-      placeId: combinedMapObject.currentLocation.id,
+      placeId: combinedMapObject.currentLocation.placeId,
       fields: ['photo', 'name', 'place_id', 'formatted_address', 'opening_hours', 'rating']
     };
 
-    combinedMapObject.googleMap.getDetails(request, function (place, status) {
+    combinedMapObject.googleMap.getDetails(request, (place, status) => {
       if (status !== 'OK') {
         return false;
+      } else {
+        this.setState({ placeObject: place });
       }
-      // console.log(place);
-      return place;
     });
+  }
+  render() {
+    // console.log(this.state.placeObject);
+    const { placeObject } = this.state;
 
-    return (
-      <div className="main__container">
-        <Header name={this.props.view.name} prevName={this.props.view.prevName} setView={this.props.setView} isLoggedIn={this.props.isLoggedIn} />
-        <div className="main__body">
-          <div className="gym__view-container">
-            <div className="gym__view-photos"> classphotos</div>
-            <div className="gym__view-address">gym address</div>
-            <div className="gym__view-hours">gym hours</div>
-            <div className="gym__view-peak-hours">peak hours</div>
-            <button className="gym__view-button">Add Gym</button>
+    if (placeObject) {
+      return (
+        <div className="main__container">
+          <Header name={this.props.view.name} prevName={this.props.view.prevName} setView={this.props.setView} isLoggedIn={this.props.isLoggedIn} />
+          <div className="main__body">
+            <div className="gym__view-container">
+              <img className="gym__view-photos" src={placeObject.photos} />
+              <div className="gym__view-address">{placeObject.formatted_address}</div>
+              <div className="gym__view-hours">{placeObject.opening_hours.weekday_text}</div>
+              <div className="gym__view-peak-hours">peak hours</div>
+              <button className="gym__view-button">Add Gym</button>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>no data...</div>
+      );
+    }
+
   }
 }
