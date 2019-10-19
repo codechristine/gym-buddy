5981eed1d5de8a740c9ac4a5a0c4162a0490e554
NoelCarino <leononirac1992@gmail.com>
2019-10-19 00:22:14 +0000

    10/18 - 5:21pm | Back End - Started working on signup conditionals

--- server/public/api/users/user-signup.php
+++ server/public/api/users/user-signup.php
@@ -2,16 +2,46 @@
 <?php require_once('../db-connection.php') ?>
 <?php
 
-    $insertQuery = "INSERT INTO `user-signup` (`user-name`,`first-name`,`last-name`,`age`)
-                VALUES ('Aaaron', 'Noel', 'Carino', 26)";
+    $checkUserName = "killaNoel";
+    $queryCheck = "SELECT * FROM `user-signup` WHERE `user-signup`.`user-name` = $checkUserName";
+    $checkResult = mysqli_query($conn, $queryCheck);
 
-    $result = mysqli_query($conn, $insertQuery);
-
-    if (!$result){
-        echo "Failed";
-    } else {
-        echo "Success";
+    if(!$checkResult){
+        throw new Exception("Connection Error ". mysqli_connect_error());
     }
+
+    $output = [];
+    $num_rows = mysqli_num_rows($checkResult);
+
+    if($num_rows === 0 ) throw new Exception("Invalid");
+
+    while($row = mysqli_fetch_assoc($checkResult)){
+        array_push($output, $row);
+    }
+
+    $json_version = json_encode($output);
+    print($json_version);
+
+
+
+
+
+    // $insertQuery = "INSERT INTO `user-signup` (`user-name`,`first-name`,`last-name`,`age`)
+    //             VALUES ('Aaaron', 'Noel', 'Carino', 26)";
+
+    // $result = mysqli_query($conn, $insertQuery);
+
+    // if (!$result){
+    //     echo "Failed";
+    // } else {
+    //     echo "Success";
+    // }
+
+
+
+
+
+
 
     // $signUpQuery = "SELECT * FROM `user-signup`";
 
