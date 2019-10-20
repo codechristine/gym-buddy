dc56e4c12d675ae73ec9f4babaa8a91df81d4364
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-20 02:33:18 +0000

    Added simple loading animation when pressing on current location, only button that sometimes delivers a long enough time to warrant a loading animation

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -6,7 +6,8 @@ import Header from './header';
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.updateLocation = this.updateLocation.bind(this);
+    this.state = { isLoading: false };
+    this.updateLoading = this.updateLoading.bind(this);
   }
   updateLocation() {
     navigator.geolocation.getCurrentPosition(position => {
@@ -14,19 +15,29 @@ class Home extends React.Component {
       this.props.setLocation(locationObj);
       this.props.setView('map', 'home', {});
     });
-
+  }
+  updateLoading() {
+    this.setState({ isLoading: true });
+    this.updateLocation();
   }
   render() {
     const viewMap = () => this.props.setView('map', 'home', {});
     const searchOptions = { componentRestrictions: { country: 'us' } };
+    const { isLoading } = this.state;
+    let element;
+
+    if (!isLoading) {
+      element = <button className="main__view" onClick={this.updateLoading}> View Gyms Around Me </button>;
+    } else {
+      element = <div className="lds-ring"><div></div><div></div><div></div><div></div></div>;
+    }
+
     return (
       <div className="main__container">
         <Header name = { this.props.view.name } prevName = { this.props.view.prevName } setView = { this.props.setView } isLoggedIn = { this.props.isLoggedIn } />
         <div className="main__body">
           <Carousel />
-          <button className="main__view" onClick={this.updateLocation}>
-            View Gyms Around Me
-          </button>
+          { element }
           <LocationSearchInput setLocation = { this.props.setLocation } viewMap={ viewMap } searchOptions={ searchOptions }/>
         </div>
       </div>
