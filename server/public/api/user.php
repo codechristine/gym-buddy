7bd0f934c413973dcbea84b5bbe410e88b01d65c
NoelCarino <leononirac1992@gmail.com>
2019-10-22 01:01:42 +0000

    10/21 - 6:01pm | Back End - Sign Up user through Postman - Success

--- server/public/api/user.php
+++ server/public/api/user.php
@@ -1,30 +1,19 @@
 <?php
 
-require_once('functions.php');
-require_once('db-connection.php');
+    require_once('functions.php');
+    require_once('db-connection.php');
+    startup();
+    set_exception_handler('error_handler');
 
-startup();
-set_exception_handler('error_handler');
-
-switch($_SERVER['REQUEST_METHOD']){
-    case 'POST':
-        // POST is used to add user to database
-        //      [x] 1 - check if user exist
-        //      [ ] 2 - if user DNE, query to add user to database
-        //      [ ] 3 - if user user exist, THROW AN ERROR
-        // require_once('user-add.php');    
-        // require_once('user-get.php');
-        $userToAdd = $_GET['user-name'];
-        require_once('user-check.php');
-        print("user.php - " . $num_rows);
-        print('check 1');
-        break;
-    case 'GET':
-        require_once('user-get.php');
-        break;
-}
-
-
-
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'POST':
+            require_once('user-check.php');
+            require_once('user-signup.php');
+            // print $hello;
+            break;
+        case 'GET':
+            require_once('user-get.php');
+            break;
+    }
 
 ?>
