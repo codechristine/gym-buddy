b2af578b60a5cfe5e827ac43eb1b9484baefd402
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-23 21:36:58 +0000

    Defined initial component skeleton for gym buddy profile along with styling

---
+++ client/components/gym-buddy.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+class GymBuddy extends React.Component {
+  render() {
+    return (
+      <h1>test</h1>
+    );
+  }
+}
+
+export default GymBuddy;
+/*
+<div class="main">
+      <div class="main__container">
+        <div class="main__header">
+          <button class="btn main__button">Sign Up</button>
+          <button class="btn main__button">Log In</button>
+        </div>
+        <div class="main__body">
+          <div class="buddy__container">
+            <div class="buddy__container-top">
+              <div class="buddy__container-photo">
+                <div class="buddy__photo">
+                  <img src="https://static.thenounproject.com/png/538846-200.png" class="buddy__picture" />
+                </div>
+                <div class="buddy__info">
+                   <div class="buddy__info-buttons">
+                      <button class="btn buddy__button">Add</button>
+                   </div>
+                   <div class="buddy__info-name">
+                      <div class="buddy__name">Aaron Domingo</div>
+                      <div class="buddy__gym">Merage Jewish Community Center of Orange County</div>
+                   </div>
+                </div>
+              </div>
+              <div class="buddy__container-stats">
+                <div class="buddy__stats-header">
+                  Stats
+                </div>
+                <div class="buddy__stats-card">
+                  <div class="buddy__stats-category">
+                    WeightLifting:
+                  </div>
+                  <div class="buddy__stats-value">
+                    Intermediate
+                  </div>
+                </div>
+                <div class="buddy__stats-card">
+                  <div class="buddy__stats-category">
+                    WeightLifting:
+                  </div>
+                  <div class="buddy__stats-value">
+                    Intermediate
+                  </div>
+                </div>
+                <div class="buddy__stats-card">
+                  <div class="buddy__stats-category">
+                    WeightLifting:
+                  </div>
+                  <div class="buddy__stats-value">
+                    Intermediate
+                  </div>
+                </div>
+                <div class="buddy__stats-card">
+                  <div class="buddy__stats-category">
+                    WeightLifting:
+                  </div>
+                  <div class="buddy__stats-value">
+                    Intermediate
+                  </div>
+                </div>
+                <div class="buddy__stats-card">
+                  <div class="buddy__stats-category">
+                    WeightLifting:
+                  </div>
+                  <div class="buddy__stats-value">
+                    Intermediate
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="buddy__container-bottom">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+*/
