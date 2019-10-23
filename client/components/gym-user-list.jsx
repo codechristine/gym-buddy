ef1206d24ce8fbb1870fdfb6a1bf7bbcb0dbdb93
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-23 00:34:57 +0000

    Added styling for new gym user list component that makes it different from gym buddy list

--- client/components/gym-user-list.jsx
+++ client/components/gym-user-list.jsx
@@ -3,9 +3,20 @@ import React from 'react';
 class GymUserListItem extends React.Component {
   render() {
     const userInfo = this.props.userInfo;
+    let photo = userInfo.photo;
 
+    if (!photo) {
+      photo = 'https://static.thenounproject.com/png/538846-200.png';
+    }
     return (
-      <h1> {userInfo.firstname} </h1>
+      <div className="gymuser__card">
+        <div className="gymuser__card-picture">
+          <img src={photo} alt={userInfo.firstname} className="gymuser__card-photo" />
+        </div>
+        <div className="gymuser__card-name">
+          {userInfo.username}
+        </div>
+      </div>
     );
   }
 }
