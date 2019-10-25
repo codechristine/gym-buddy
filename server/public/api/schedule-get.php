98965be454f01e3772fd53ceda6420ecd0049f0e
aaronmdomingo <aaronmdomingo@gmail.com>
2019-10-25 17:31:46 +0000

    Added dummy data for the schedule, confirmed on postman that the response is accurate based on the request type

--- server/public/api/schedule-get.php
+++ server/public/api/schedule-get.php
@@ -1,22 +1,22 @@
 <?php
 
-    $rawdata = getBodyData();
-    $username = $rawdata["username"];
+    // $rawdata = getBodyData();
+    // $username = $rawdata["username"];
 
-    $queryCheck = "SELECT * FROM `schedule` WHERE `schedule`.`username` = '$username' ";
+    // $queryCheck = "SELECT * FROM `schedule` WHERE `schedule`.`username` = '$username' ";
 
-    $checkResult = mysqli_query($conn, $queryCheck);
+    // $checkResult = mysqli_query($conn, $queryCheck);
 
-    if(mysqli_num_rows($checkResult) === 0){
-        throw new Exception("Cannot Find Schedule with given username");
-    } else {
-        $output = [];
-        while ($row = mysqli_fetch_assoc($checkResult)){
-            $output[] = $row;
-        }
-        $json_output = json_encode($output);
-        print($json_output);
-    }
-    
+    // if(mysqli_num_rows($checkResult) === 0){
+    //     throw new Exception("Cannot Find Schedule with given username");
+    // } else {
+    //     $output = [];
+    //     while ($row = mysqli_fetch_assoc($checkResult)){
+    //         $output[] = $row;
+    //     }
+    //     $json_output = json_encode($output);
+    //     print($json_output);
+    // }
+    readfile('schedule-dummy-data.json');
 
 ?>
