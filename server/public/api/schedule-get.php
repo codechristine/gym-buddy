d3f64a1a14bb3ebb64e168137270ef8324506fa3
NoelCarino <leononirac1992@gmail.com>
2019-10-26 00:15:19 +0000

    10/25 - 5:14pm | Back End - Schedule get success

--- server/public/api/schedule-get.php
+++ server/public/api/schedule-get.php
@@ -1,22 +1,58 @@
 <?php
 
-    // $rawdata = getBodyData();
-    // $username = $rawdata["username"];
+    $rawdata = getBodyData();
+    $currentuser = $rawdata["username"];
 
-    // $queryCheck = "SELECT * FROM `schedule` WHERE `schedule`.`username` = '$username' ";
+    $query = "SELECT DISTINCT `schedule`.`username` as user,`schedule`.`day` as week_day,
+    (
+        SELECT 
+        GROUP_CONCAT(DISTINCT `schedule`.`starttime`, ' ', `schedule`.`endtime` SEPARATOR ' ')  
+        FROM `schedule`
+        WHERE `schedule`.`day` = week_day AND `schedule`.`username` = user
+    ) as `schedule`
+    FROM `schedule`
+    WHERE `schedule`.`username` = '$currentuser'";
 
-    // $checkResult = mysqli_query($conn, $queryCheck);
+    $result = mysqli_query($conn, $query);
 
-    // if(mysqli_num_rows($checkResult) === 0){
-    //     throw new Exception("Cannot Find Schedule with given username");
-    // } else {
-    //     $output = [];
-    //     while ($row = mysqli_fetch_assoc($checkResult)){
-    //         $output[] = $row;
-    //     }
-    //     $json_output = json_encode($output);
-    //     print($json_output);
-    // }
-    readfile('schedule-dummy-data.json');
+    $output = [
+        "Sunday" => [],
+        "Monday" => [],
+        "Tuesday" => [],
+        "Wednesday" => [],
+        "Thursday" => [],
+        "Friday" => [],
+        "Saturday" => []
+    ];
 
+    while($row = mysqli_fetch_assoc($result)){
+            $pizza = $row["schedule"];
+            $pieces = array_map('intval', explode(' ', $pizza));
+        switch($row["week_day"]){
+            case $row["week_day"] === "Sunday":
+                $output[$row["week_day"]] = $pieces = array_map('intval', explode(' ', $pizza));
+                break;
+            case $row["week_day"] === "Monday":
+                $output[$row["week_day"]] = $pieces = array_map('intval', explode(' ', $pizza));
+                break;
+            case $row["week_day"] === "Tuesday":
+                $output[$row["week_day"]] = $pieces = array_map('intval', explode(' ', $pizza));
+                break;
+            case $row["week_day"] === "Wednesday":
+                $output[$row["week_day"]] = $pieces = array_map('intval', explode(' ', $pizza));
+                break;
+            case $row["week_day"] === "Thursday":
+                $output[$row["week_day"]] = $pieces = array_map('intval', explode(' ', $pizza));
+                break;
+            case $row["week_day"] === "Friday":
+                $output[$row["week_day"]] = $pieces = array_map('intval', explode(' ', $pizza));
+                break;
+            case $row["week_day"] === "Saturday":
+                $output[$row["week_day"]] = $pieces = array_map('intval', explode(' ', $pizza));
+                break;
+        }
+    }
+
+        $json_output = json_encode($output);
+        print($json_output);
 ?>
